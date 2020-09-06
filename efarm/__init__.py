74bd4c633323058f33516b01092b5a5e197de591
Victor Baraka <barakavictor62@gmail.com>
2020-09-06 22:52:27 +0300

    chore(style): fix some formatting issues

--- efarm/__init__.py
+++ efarm/__init__.py
@@ -1,12 +1,9 @@
 from flask import Flask
 
 def create_app(config=None):
-    
     app = Flask(__name__, instance_relative_config=True)
-    
     app.config.from_pyfile(config)
 
     from . import main
-    app.register_blueprint(main.bp)  
-
+    app.register_blueprint(main.bp)
     return app
